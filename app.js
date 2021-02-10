d87afcde8f0d294f4fa90bcbb7b49249dbcc74ea
Matt Gross <mbgross111@gmail.com>
2021-02-10 15:02:15 -0700

    add proptProject() allowing user to enter as many projects as they like after entering profile data

--- app.js
+++ app.js
@@ -30,4 +30,59 @@ const promptUser = () => {
   );
 };
 
-promptUser().then(answers => console.log(answers));
+const promptProject = (portfolioData) => {
+  if(!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'nombre',
+        message: 'What is the name of your project'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['HTML', 'CSS', 'Bootstrap', 'Foundation', 'JavaScript', 'ES6', 'jQuery', 'Moment', 'Node']
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ]
+  ).then(projectData => {
+    portfolioData.projects.push(projectData);
+    if(projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+
+  });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
